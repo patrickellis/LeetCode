fe92f27aac66427cf509c2d62b0f30f3eab909bb
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-05-28 13:28:07 +0100

    Time: 89 ms (21.54%), Space: 14.4 MB (57.75%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # SOLUTIONS --- 
+        # Two pointers:
+        # one moves from left to right, the other from right to left
+        # increment left pointer and decrement right pointer until ! l < r
+        l = 0
+        r = len(s)-1
+        while(l < r):
+            while (s[l] == " " or not s[l].isalnum()) and l < r:
+                l+=1                
+            while (s[r] == " " or not s[r].isalnum()) and r > l:
+                r-=1                
+            if(s[l].lower() != s[r].lower()):
+                return False
+            l+=1
+            r-=1
+        return True
