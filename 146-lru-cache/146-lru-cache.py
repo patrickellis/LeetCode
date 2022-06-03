b279d87a90781f1e09715176c7f938e87704fbb0
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-06-03 14:08:34 +0100

    Time: 2112 ms (8.91%), Space: 74.8 MB (88.26%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,30 +1,32 @@
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cap = capacity
+        self.capacity = capacity
+        self.curr_cap = 0
         self.cache = dict()
-        self.head = ListNode(-1, -1)
-        self.tail = ListNode(-1, -1)
+        # for reasons that will become clear later,
+        # we sandwich our actual list with these bookmark nodes
+        self.head = ListNode(-1,-1)
+        self.tail = ListNode(-1,-1)
         self.head.next = self.tail
         self.tail.prev = self.head
-        self.curr_cap = 0
 
     def get(self, key: int) -> int:
-        if not key in self.cache:
-            return -1
-        # remove key from list
-        # insert into head
-        # update self.cache to point to head
-        self.put(key, self.cache[key].val)
-        return self.cache[key].val
-        
+        if not key in self.cache: return -1
+        node = self.cache[key]
+        self.put(key,node.val)
+        return node.val
 
     def put(self, key: int, value: int) -> None:
+        # if the key exists in our cache, remove it
+        # if current capacity >= maximum capacity, remove LRU item
+        # insert the item into the head of our list
+        # insert the item into the cache
         if key in self.cache:
-            self.deleteNode(key)
-        if self.curr_cap >= self.cap:
-            self.deleteNode()
-        
+            self.remove(key)
+        if self.curr_cap >= self.capacity:
+            self.remove()
+            
         node = ListNode(key,value)
         node.next = self.head.next
         node.prev = self.head
@@ -32,24 +34,29 @@ class LRUCache:
         self.head.next = node
         self.curr_cap += 1
         self.cache[key] = node
-
-    def deleteNode(self, key=None):
-        if self.head == self.tail.prev:
-            return
         
-        if key is None:
+
+    def remove(self, key = None):
+        # guard, we don't want to remove our bookmarks
+        # under any circumstances
+        if self.head == self.tail.prev: return        
+        if key == None:
             node = self.tail.prev
         else:
             node = self.cache[key]
-            
+        
+        # remove from list
         node.prev.next = node.next
         node.next.prev = node.prev
-        node.prev = None
+        # not 100% convinced this is necessary due to garbage collection
+        # once no other nodes point to 'node'
         node.next = None
-        #self.cache.pop(node.key, None)
-        del self.cache[node.key]
+        node.prev = None
+        # remove from cache
+        self.cache.pop(node.key, None)
         self.curr_cap -= 1
         
+
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
