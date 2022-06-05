a8fe804450378e99e7b3ada0ba10de0da703ac7c
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-06-05 13:20:31 +0100

    Time: 742 ms (5.02%), Space: 21.1 MB (80.80%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,13 +1,35 @@
+def print_array(arr):
+    print("printing array")
+    for i in arr:
+        print("{} ".format(i), end="", flush=True)
+        
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        leftProducts = list(nums)
-        rightProducts = list(nums)
-        for i in range(1,len(nums)):
-            leftProducts[i]*=leftProducts[i-1]
-            right_index = len(nums)-1-i
-            rightProducts[right_index]*=rightProducts[right_index+1]
-        for i in range(1,len(nums)-1):
-            nums[i] = leftProducts[i-1]*rightProducts[i+1]
-        nums[0]=rightProducts[1]
-        nums[-1]=leftProducts[-2]
-        return nums
+        # copy nums for reference
+        products = list(nums)
+        # for readability, assign the length of nums to a variable
+        n = len(nums)
+        
+        # calculate the products of all numbers to the left of index i
+        for i in range(1,n-1):
+            products[i] *= products[i-1]
+        
+        # Now, from right to left, calculate the right products and update products array
+        right_product = 1
+        
+        for i in range(n-1,-1,-1):
+            if i==n:
+                products[i] = products[i-1]
+            elif i==0:
+                products[i] = right_product
+            else:
+                products[i] = products[i-1] * right_product
+            print("i: {}, products[i]: {}, right_product: {}".format(
+                i,
+                products[i],
+                right_product
+            ))
+            right_product *= nums[i]
+        print_array(products)
+        
+        return products
