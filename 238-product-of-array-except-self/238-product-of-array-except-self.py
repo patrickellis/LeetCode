9e48b5d5ca082e72fa27ce3dea57327b434b83d1
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-06-05 13:20:48 +0100

    Time: 344 ms (36.72%), Space: 21.1 MB (80.80%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.py
+++ 238-product-of-array-except-self/238-product-of-array-except-self.py
@@ -1,8 +1,3 @@
-def print_array(arr):
-    print("printing array")
-    for i in arr:
-        print("{} ".format(i), end="", flush=True)
-        
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         # copy nums for reference
@@ -24,12 +19,6 @@ class Solution:
                 products[i] = right_product
             else:
                 products[i] = products[i-1] * right_product
-            print("i: {}, products[i]: {}, right_product: {}".format(
-                i,
-                products[i],
-                right_product
-            ))
-            right_product *= nums[i]
-        print_array(products)
+            right_product *= nums[i]        
         
         return products
