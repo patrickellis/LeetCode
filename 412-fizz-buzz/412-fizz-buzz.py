6b613138c79e2c264f036a0cb85e3723983394b8
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-06-08 23:46:23 +0100

    Time: 56 ms (55.85%), Space: 15.2 MB (16.45%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,8 +1,3 @@
 class Solution:
-    def fizzBuzz(self, n: int) -> List[str]:
-        res = [str(i+1) for i in range(n)]
-        for i in range(2,n,3): res[i] = "Fizz"
-        for i in range(4,n,5): res[i] = "Buzz"
-        for i in range(14,n,15): res[i] = "FizzBuzz"
-        
-        return res
+    def fizzBuzz(self, n):
+        return ['Fizz' * (not i % 3) + 'Buzz' * (not i % 5) or str(i) for i in range(1, n+1)]
