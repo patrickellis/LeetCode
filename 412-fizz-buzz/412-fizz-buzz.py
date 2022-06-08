59e7c34f6c018cdd560814966737149e53b5ad6a
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-06-08 23:44:23 +0100

    Time: 59 ms (49.85%), Space: 15.1 MB (42.60%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,28 +1,12 @@
-import threading
-
-class FizzBuzz(threading.Thread):
-    def __init__(self, printer, gap, bound, result):
-        threading.Thread.__init__(self)
-        self.printer = printer
-        self.gap = gap
-        self.bound = bound
-        self.result = result
-        self.current = gap-1
-
-    def run(self):
-        while self.current < self.bound:
-            self.result[self.current] = self.printer(self.current+1)
-            self.current += self.gap
-
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
-        result = list(map(str, range(1, n+1)))
-        threads = [
-            FizzBuzz(lambda i: 'FizzBuzz' if i % 5 == 0 else 'Fizz', 3, n, result),
-            FizzBuzz(lambda i: 'FizzBuzz' if i % 3 == 0 else 'Buzz', 5, n, result),
-        ]
-        for t in threads:
-            t.start()
-        for t in threads:
-            t.join()
-        return result
+        res = [str(i+1) for i in range(n)]
+        for i in range(2,n,3):
+            print(i)
+            res[i] = "Fizz"
+        for i in range(4,n,5):
+            res[i] = "Buzz"
+        for i in range(14,n,15):
+            res[i] = "FizzBuzz"
+        
+        return res
