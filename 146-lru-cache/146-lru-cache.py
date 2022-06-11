07cc0c7845bd2aea028fab32319dae57fbcb9bf5
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-06-11 13:55:42 +0100

    Time: 1677 ms (13.87%), Space: 76.5 MB (21.70%) - LeetHub

--- 146-lru-cache/146-lru-cache.py
+++ 146-lru-cache/146-lru-cache.py
@@ -1,60 +1,86 @@
-class LRUCache:
+"""
+always add the node to the tail position
+remove node from the linked list from the head next position
 
-    def __init__(self, capacity: int):
-        self.capacity = capacity
-        self.curr_cap = 0
-        self.head = ListNode(-1,-1)
-        self.tail = ListNode(-1,-1)
-        self.head.next = self.tail
-        self.tail.prev = self.head
-        self.cache = dict()
-        
+everytime I call get, I remove the node and add the node to the tail
 
-    def get(self, key: int) -> int:
-        if not key in self.cache:
-            return -1
-        cache_result = self.cache[key].val
-        self.put(key, cache_result)
-        return cache_result
+if I call put, I just add the node to the tail, if existes the same key, remove then added to the tail
 
-    def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            self.remove(key)
-        
-        if self.curr_cap >= self.capacity:
-            self.remove()
-        
-        node = ListNode(key, value)
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next.prev = node
-        self.head.next = node
-        self.cache[key] = node
-        self.curr_cap += 1
-        
-    def remove(self, key=None):
-        if self.head.next == self.tail:
-            return
-        if key == None:
-            node = self.tail.prev
-        else:
-            node = self.cache[key]
-        
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        self.cache.pop(node.key, None)
-        self.curr_cap -= 1
-
-
-class ListNode:
-    def __init__(self, key, value):
-        self.key = key
-        self.val = value
-        self.prev = None
-        self.next = None
-        
+head <-> [1,1] <-> tail 
+"""
 
 # Your LRUCache object will be instantiated and called as such:
 # obj = LRUCache(capacity)
 # param_1 = obj.get(key)
 # obj.put(key,value)
+
+class Node:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+    
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.dic = dict()
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    """
+    remove and add the node to the end, means that is recently used node
+    also return the value by check in the dic
+    """
+    def get(self, key):
+        if key in self.dic:
+            n = self.dic[key]
+            self._remove(n)
+            self._add(n)
+            return n.val
+        return -1
+
+    """
+    set the value if exists in dic, remove then add
+    otherwise create a new one then added to the tail
+    if len(self.dic) > self.capacity:
+        remove LRU node
+    """
+    def put(self, key, value):
+        if key in self.dic:
+            self._remove(self.dic[key])
+        n = Node(key,value)
+        self._add(n)
+        self.dic[key] = n
+        if len(self.dic) > self.capacity:
+            n = self.head.next
+            self._remove(n)
+            del self.dic[n.key]
+
+    """
+    head <-> [1,1] <-> tail 
+     p     <->            n     
+    """
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    """
+    add the node to the tail 
+    head <-> [1,1]                    <-> tail 
+                p   <- self.tail.pre
+                p  <-> node     <-> tail
+
+    """
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
+    
+    
