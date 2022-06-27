5888c03570cca1ac057cf92844912af5eca718e2
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-06-27 17:50:55 +0100

    Time: 1065 ms (46.26%), Space: 27.9 MB (38.70%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,9 +1,14 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = -pow(10,5)
-        running_sum = -pow(10,5)        
-        it = 0
-        for num in nums:
-            running_sum = max(num, running_sum + num)
-            res = max(running_sum, res)
+        # For as long as the current sum is non-negative
+        # we are better off continuing the previous sum than beginning
+        # a new one. 
+        res = -float('inf')
+        running_sum = 0
+        
+        for n in nums:
+            reset_sum = n
+            continued_sum = running_sum + n
+            running_sum = continued_sum if running_sum >= 0 else reset_sum
+            res = max(res,running_sum)
         return res
