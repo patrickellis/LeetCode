27b71ec89713e9d0ccdb927b65ffa27955d340fd
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-07-02 22:24:51 +0100

    Time: 5000 ms (51.78%), Space: 67.7 MB (50.69%) - LeetHub

--- 204-count-primes/204-count-primes.py
+++ 204-count-primes/204-count-primes.py
@@ -5,12 +5,10 @@ class Solution:
         prime = [True for i in range(n)]
         prime[0] = prime[1] = False        
         res = 1
-        upperLim = ceil(n ** 0.5)
         for i in range(3, n, 2):
             if prime[i]:
                 res += 1
-                if i > upperLim: 
-                    continue
+
                 for j in range(i*i, n, i):
                     prime[j] = False
         return res
