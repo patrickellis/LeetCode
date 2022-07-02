67c126cc77f9fd98a090af034ac4cf7fdd6c18ff
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-07-03 00:04:14 +0100

    Time: 107 ms (61.13%), Space: 17.5 MB (49.27%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -6,8 +6,8 @@ class Solution:
         for i in intervals:
             if heap and heap[0] <= i[0]:
                 # If the new start time is greater than or equal to the exist end time, means the room has been released, replace the previous time with the new ending time
-                heapq.heapreplace(heap, i[-1])
+                heapq.heapreplace(heap, i[1])
             else:
                 # The room is still in use, add (push a new end time to min heap) a new room
-                heapq.heappush(heap, i[-1])
+                heapq.heappush(heap, i[1])
         return len(heap)
