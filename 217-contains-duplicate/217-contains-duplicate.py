ca2d8a2562ccfc4e5a6a8f5fac8cb73b0d4844d7
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-07-04 00:08:00 +0100

    Time: 461 ms (96.52%), Space: 26 MB (72.20%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.py
+++ 217-contains-duplicate/217-contains-duplicate.py
@@ -1,5 +1,9 @@
 class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:        
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        # Solution 1: O(nlogn)
+        # sort the array and compare adjacent elements for equality
+        # Solution 2: O(n)
+        # Insert into a set, check for existence as we go
         seen = set()
         for n in nums:
             if n in seen: return True
