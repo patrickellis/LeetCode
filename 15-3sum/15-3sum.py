fcee69744e273f1b790aea0a123c884a7688c839
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-07-03 15:00:50 +0100

    Time: 1411 ms (45.97%), Space: 18.1 MB (72.24%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,32 +1,34 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        # in order to solve this, 
-        # we should fix one number
-        # and then search the rest of the array for comlpementing
-        # pairs that together equal 0 
-        # we know from two sum that we can find two nums in an array that equal
-        # a target in O(n) time
-        # which makes the total complexity O(N^2)
-        nums.sort()
+        # We know that our algorithm is going to run in 
+        # O(N^2) time
+        # Therefore, sorting the input comes at no cost
+        nums.sort() 
         res = []
-        index = 0
-        while index < len(nums) - 2:
-            # skip duplicates
-            while index > 0 and index < len(nums) and nums[index] == nums[index-1]:
-                index += 1
-            l = index+1; r = len(nums)-1
-            while l < r:
-                triplet_sum = nums[index] + nums[l] + nums[r]                
-                if triplet_sum > 0:
-                    r -= 1
-                elif triplet_sum < 0:
-                    l += 1
-                else:
-                    res.append([nums[index],nums[l],nums[r]])    
-                    # skip duplicates
-                    while l < r and nums[l]==nums[l+1]: l+=1
-                    while r > l and nums[r]==nums[r-1]: r-=1
-                    l+=1; r-=1;
-            index +=1
+        n = len(nums)
+        i = 0
+        while(i < n):
+            # skip duplicates for first number
+            while i > 0 and i < n and nums[i] == nums[i-1]: 
+                i+=1
             
+            # initialise left and right pointers
+            l, r = i+1, n-1
+            
+            while l < r:
+                total_sum = nums[i] + nums[l] + nums[r]
+                if total_sum < 0:
+                    l+=1
+                elif total_sum > 0:
+                    r-=1
+                else:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l+=1; r-=1;
+                    while l < r and nums[l] == nums[l-1]:
+                        l+=1
+                    while r > l and nums[r] == nums[r+1]:
+                        r-=1
+            i+=1
         return res
+            
+        
