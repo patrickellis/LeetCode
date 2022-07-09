3683bbc34cb48fe5cffdf01dba0f674e245ccc82
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-07-10 00:06:49 +0100

    Time: 267 ms (76.88%), Space: 25.4 MB (75.37%) - LeetHub

--- 189-rotate-array/189-rotate-array.py
+++ 189-rotate-array/189-rotate-array.py
@@ -4,7 +4,7 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         # apply fix for if k > len(nums)
-        k = k % len(nums)
+       # k = k % len(nums)
         res = []
         
         #  nums = [1,2,3,4,5,6,7], k = 3, len(nums) = 7
