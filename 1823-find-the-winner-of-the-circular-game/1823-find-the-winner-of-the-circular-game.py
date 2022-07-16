342d9bcf6b616c49af00b4cb6432a7ff34e4a4c2
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-07-16 21:29:40 +0100

    Time: 2727 ms (5.00%), Space: 13.9 MB (42.93%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -3,7 +3,6 @@ class Solution:
         players = set()
         for i in range(n):
             players.add(i+1)
-        #print(list(dict.fromkeys(players)))
         player = 0
         count = k
         while len(players) > 1:            
@@ -12,12 +11,10 @@ class Solution:
                 player = player % n
             if count == 1:
                 if player in players:
-                    print("removing player: {}".format(player))
                     players.remove(player)
                     count = k                    
                 continue
             if player in players:
-                print("here")
                 count-=1
         
         return players.pop()
