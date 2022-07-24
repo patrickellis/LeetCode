2ec2e7a5c5bce315e5f3be59d4607e29341117b9
Patrick Ellis <74929696+patrickellis@users.noreply.github.com>
2022-07-24 16:21:32 +0100

    Time: 1702 ms (37.51%), Space: 18 MB (72.14%) - LeetHub

--- 15-3sum/15-3sum.py
+++ 15-3sum/15-3sum.py
@@ -1,35 +1,28 @@
-# Time Complexity: O(N^2) - for each number, run a two pointers algorithm on the remainder of the
-# input array.
-# Space Compleixty: O(1) - no data structures required except for the array holding the answer
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:        
-        # We know that our algorithm is going to run in 
-        # O(N^2) time
-        # Therefore, sorting the input comes at no cost
-        nums.sort() 
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        n = len(nums)
         res = []
- 
         
-        for i,val in enumerate(nums):
-            if i > 0 and val == nums[i-1]:
+        for i in range(n):
+            if i > 0 and nums[i] == nums[i-1]:
                 continue
             
-            # initialise left and right pointers
-            l, r = i+1, len(nums)-1
+            l = i+1; r = n-1;
             
             while l < r:
                 total_sum = nums[i] + nums[l] + nums[r]
+                
                 if total_sum < 0:
-                    l+=1
+                    l += 1
                 elif total_sum > 0:
-                    r-=1
+                    r -= 1
                 else:
-                    res.append([nums[i], nums[l], nums[r]])
-                    l+=1; r-=1;
-                    while l < r and nums[l] == nums[l-1]:
-                        l+=1
-                    while r > l and nums[r] == nums[r+1]:
-                        r-=1            
+                    res.append([nums[i],nums[l],nums[r]])
+                    r-=1
+                    l+=1
+                    while nums[l] == nums[l-1] and l < r:
+                        l += 1
+                    while nums[r] == nums[r+1] and l < r:
+                        r -= 1
         return res
-            
-        
